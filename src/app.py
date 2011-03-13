e7c39dfc2c00dc21775ada96e0234216559278c9
Greg Aker <greg@gregaker.net>
2011-03-13 14:30:54 -0500

    setting up app.py and urls.py

--- src/app.py
+++ src/app.py
@@ -1,19 +1,3 @@
-#!/usr/bin/env python
-#
-# Copyright 2009 Facebook
-#
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
-
 import tornado.auth
 import tornado.escape
 import tornado.httpserver
@@ -25,61 +9,20 @@ from tornado.options import define, options
 
 define("port", default=8000, help="run on the given port", type=int)
 
+BASEPATH = os.path.abspath(os.path.dirname(__file__))
 
 class Application(tornado.web.Application):
     def __init__(self):
-        handlers = [
-            (r"/", MainHandler),
-            # (r"/auth/login", AuthHandler),
-            # (r"/auth/logout", LogoutHandler),
-        ]
         settings = dict(
-            cookie_secret="32oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=",
+            xheaders=True,
+            xsrf_cookie=True,
+            debug=True,
+            template_path=os.path.join(BASEPATH, 'templates'),
+            static_path=os.path.join(BASEPATH, 'media'),
+            cookie_secret="$@(*YFDKHjdsaf afslkajhsdfghkasjdgtais)/Vo=",
             login_url="/auth/login",
         )
-        tornado.web.Application.__init__(self, handlers, **settings)
-
-
-class BaseHandler(tornado.web.RequestHandler):
-    pass
-    # def get_current_user(self):
-    #     user_json = self.get_secure_cookie("user")
-    #     if not user_json: return None
-    #     return tornado.escape.json_decode(user_json)
-
-
-class MainHandler(BaseHandler):
-    # @tornado.web.authenticated
-    def get(self):
-        self.write('hi!')
-        # print self.get_current_user()
-        # name = tornado.escape.xhtml_escape(self.current_user["name"])
-        # self.write("Hello, " + name)
-        # self.write("<br><br><a href=\"/auth/logout\">Log out</a>")
-
-
-# class AuthHandler(BaseHandler, tornado.auth.GoogleMixin):
-#     @tornado.web.asynchronous
-#     def get(self):
-#         # pass
-#         if self.get_argument("openid.mode", None):
-#             self.get_authenticated_user(self.async_callback(self._on_auth))
-#             return
-#         self.authenticate_redirect()
-#     
-#     def _on_auth(self, user):
-#         if not user:
-#             raise tornado.web.HTTPError(500, "Google auth failed")
-#         
-#         print user
-#         self.set_secure_cookie("user", tornado.escape.json_encode(user))
-#         self.redirect("/")
-# 
-# class LogoutHandler(BaseHandler):
-#     def get(self):
-#         # self.clear_cookie("user")
-#         self.clear_all_cookies()
-#         self.redirect("/")
+        tornado.web.Application.__init__(self, urls.url_list, **settings)
 
 def main():
     tornado.options.parse_command_line()
---
+++ src/urls.py
@@ -0,0 +1,5 @@
+
+
+url_list = [
+    (r'/', HomeHandler),
+]
