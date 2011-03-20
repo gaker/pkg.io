9f079dea3148083bfcfa9a092b5898219016ef1d
Greg Aker <greg@gregaker.net>
2011-03-20 12:03:36 -0500

    Removing the error handler we don't need.

--- src/handlers/error.py
+++
@@ -1,8 +0,0 @@
-import tornado.web
-from handlers import BaseHandler
-
-class ErrorHandler(BaseHandler):
-    pass
-    # def get(self):
-    #     print dir(self)
-        
--- src/urls.py
+++ src/urls.py
@@ -1,9 +1,9 @@
-from handlers.error import ErrorHandler
+# from handlers.error import ErrorHandler
 from handlers.home import HomeHandler
 
-# from handlers import BaseHandler
+from handlers import BaseHandler
 
 url_list = [
     (r'/', HomeHandler),
-    (r'/.*/?', ErrorHandler)
+    (r'/.*/?', BaseHandler)
 ]
