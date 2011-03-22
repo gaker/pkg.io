2a54ce4fa355537a94f92af3f3fd8d2ff27188d3
Pascal Kriete <pascal@pascalkriete.com>
2011-03-21 23:14:18 -0400

    More tweaking and the 50 lines of javascript that took me half an iceage to write

--- src/media/js/package_builder.js
+++ src/media/js/package_builder.js
@@ -30,29 +30,79 @@
 		}
 	});
 	
-	$('#extension_hooks').change(function () {
+	
+	// Iteration 823098589409 of death ... works for now
+	
+	var Hooks = (function() {
 		
 		var container = $('#extension_hook_options'),
-			values = $(this).val(),
-			s = ''; // @todo, language keys
+			hook_checks = $('input[name="extension_hooks[]"]'),
+			hook_inputs = [];
 		
-		if (values === null) { 
-			container.hide();
-			return false;
-		}
+		// We want to maintain the same order for the textfields as
+		// we do for the checkboxes, sooo .... we keep the checkbox
+		// index (which is constant) for each checkbox we add in a
+		// sorted array (hook_inputs).
 		
-		container.show();
-		
-		values.forEach(function (item) {
-			if (container.find('dl#hook_'+item).length === 0) {
-				s += '<dl>';
-				s += '<dt><label for="extension_hook_'+item+'">'+item+'</label></dt>';
-				s += '<dd>';
-				s += '<input class="text" type="text" name="extension_hook_'+item+'" id="extension_hook_'+item+'">';
-				s += '</dd></dl>';
+		return {
+			add: function(check_el) {
+				var idx = hook_checks.index(check_el),
+					value = check_el.value,
+					prev, str = '';
+				
+				str = '<dt><label for="extension_hook_'+value+'">'+value+'</label></dt>' +
+					  '<dd><input class="text" type="text" name="extension_hook_'+value+'" id="extension_hook_'+value+'"></dd>';
+				
+				// Add the original index to our hook input array and re-sort.
+				hook_inputs.push(idx);
+				hook_inputs.sort(function(a, b) {
+					return a - b;
+				});
+				
+				// Now extract the new index relative to the input elements
+				// already shown
+				prev = hook_inputs.indexOf(idx);
+				
+				if (prev < 1) {
+					container.find('dl').prepend(str);
+				} else {
+					container.find('dl dd').eq(prev - 1).after(str);
+				}
+
+				container.show();
+			},
+			
+			remove: function(check_el) {
+				var idx = hook_checks.index(check_el), dd;
+				idx = hook_inputs.indexOf(idx);
+				
+				if (idx !== -1) {
+					hook_inputs.splice(idx, 1);
+				}
+				
+				// Remove the dt and dd that make up our text input
+				dd = $('#extension_hook_'+check_el.value).parent();
+				dd.prev().remove();
+				dd.remove();
+				
+				if ( ! this.count()) {
+					container.hide()
+				}
+			},
+			
+			count: function() {
+				return hook_inputs.length;
 			}
-		});
+		};
 		
-		$('#extension_hook_options div').html(s);
+	})();
+	
+	$('input[name="extension_hooks[]"]').change(function () {
+		
+		if (this.checked === false) {
+			Hooks.remove(this);
+		} else {
+			Hooks.add(this);
+		}
 	});
 })(jQuery);
